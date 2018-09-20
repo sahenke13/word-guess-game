78c0afc87d1553ea3a75ac3f3d4b7cc16247c8cb
sahenke13 <shenke13@gmail.com>
2018-09-20 07:05:07 -0500

    HW3V2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 
 
 //Bank of words to randomly pick from
-var wordBank = ["cur", "peach", "deputy", "law", "sheriff", "daisy", "huckleberry", "doc holiday", "wyatt earp", "ok corral", "buffalo bill"];
+var wordBank = ["cur", "peach", "deputy", "law", "sheriff", "daisy", "huckleberry", "doc holiday", "wyatt earp", "ok corral", "buffalo bill", "billy the kid"];
 //initialize Playerwins to zero.  Keeps track of Wins.
 var playerWins = 0;
 //initialize remainingGuesses
@@ -21,6 +21,10 @@ var wrongGuess = [" "];
 
 var randomWord = "";
 
+var realAnswer = [""];
+
+var gamesWon = 0;
+
 
 //create a randomWord with function 
 function randomizer (){
@@ -33,27 +37,32 @@ function generateHiddenWord(foo){
 // Using a for loop to incorporate incorporate spaces
     for (var i=0; i < foo.length; i++){
         if(randomWord.charAt(i) == " "){
+            realAnswer[i] = "&nbsp;";
             hidenAnswer[i]= "&nbsp;";
         }
         else{
-            hidenAnswer[i] = "_";     
+            realAnswer[i] = randomWord.charAt(i);
+            hidenAnswer[i] = " _ ";     
         }
     }
+    
     currentAnswer = hidenAnswer.join(" ");
-    console.log(hidenAnswer);   
-    console.log(randomWord);
+      
+    
     document.getElementById("answerCurrent").innerHTML = currentAnswer;
 }
 
 
 //try to create a reset function
 function reset(){
+    realAnswer = [];
     hidenAnswer=[];
     generateHiddenWord(randomizer());
     remainingGuesses = 5;
     lettersGuessed = [];
     startingGuesses = 5;
     wrongGuess = [];
+    
 }
 
 //Run Generate Hidden Word Function
@@ -61,41 +70,50 @@ randomizer();
 generateHiddenWord(randomWord);
 //Take Users Guess
 
-// Next, we give JavaScript a function to execute when onkeyup event fires.
+// JavaScript a function to execute when onkeyup event fires.
 
 document.onkeyup = function(event){
     keyPressed = event.key;
     var correctGuess = false;
+   
     for (var i=0; i < randomWord.length; i++){
         if (randomWord[i] === keyPressed){
             hidenAnswer[i] = keyPressed;
             correctGuess = true;  
+           
         }  
     }
 
     //counter for remaining guesses
     if (correctGuess != true){
         lettersGuessed.push(keyPressed);
-        remainingGuesses--;
-        console.log(lettersGuessed);
+        remainingGuesses--; 
     }
         
     if (remainingGuesses > 0){
         document.getElementById("outcome").innerHTML = "Your a daisy if you do!";
-            }
+    }
     else {
         document.getElementById("outcome").innerHTML = "I'm your Huckleberry, try again!";
         reset();
     }
-    if(hidenAnswer==currentAnswer){
+
+    if(realAnswer.toString()==hidenAnswer.toString()){
+        document.getElementById("outcome").innerHTML = "Great you Win!!!";
         reset();
+        gamesWon++;
+        console.log("you win!!!");
+        document.getElementById("victories").innerHTML = gamesWon;
     }
-    console.log(currentAnswer);
+   
+    console.log(randomWord);
     console.log(hidenAnswer);
-    wrongGuess = lettersGuessed.join(" ");
-    viewedAnswer = hidenAnswer.join(" ");
+    console.log(realAnswer);
+    wrongGuess = lettersGuessed.join("");
+    viewedAnswer = hidenAnswer.join("");
     document.getElementById("guessCount").innerHTML = remainingGuesses;
     document.getElementById("answerCurrent").innerHTML = viewedAnswer;
     document.getElementById("wrongGuess").innerHTML = wrongGuess;
 }
+
 
